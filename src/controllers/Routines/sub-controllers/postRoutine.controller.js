08e21a3948abe83ca4547335cf2e6e21f332df01
rohan mainali <rohanmainali39@gmail.com>
2022-09-16 05:13:32 -0700

    post routine validation

--- src/controllers/Routines/sub-controllers/postRoutine.controller.js
+++ src/controllers/Routines/sub-controllers/postRoutine.controller.js
@@ -1,8 +1,11 @@
-const routineModel = require("../../../models/routineModel");
-const notifModel = require("../../../models/notificationModel");
-const CLASS_TYPE = require("../../../constants/index").CLASS_TYPE
-const { StatusCodes } = require("http-status-codes");
-const pusher = require("../../../utils/Socket/SocketConnection");
+const routineModel = require('../../../models/routineModel')
+const notifModel = require('../../../models/notificationModel')
+const CLASS_TYPE = require('../../../constants/index').CLASS_TYPE
+const WLV_BLOCK = require('../../../constants/index').WLV_BLOCK
+const HCK_BLOCK = require('../../../constants/index').HCK_BLOCK
+const ROUTINE_PAYLOAD = require('../../../constants/index').ROUTINE_PAYLOAD
+const { StatusCodes } = require('http-status-codes')
+const pusher = require('../../../utils/Socket/SocketConnection')
 
 const PostRoutine = async (req, res, next) => {
   //destructuring incoming data
@@ -17,98 +20,134 @@ const PostRoutine = async (req, res, next) => {
     day,
     startTime,
     endTime,
-  } = req.body;
-  console.log(group);
+  } = req.body
+
 
   //check if all attributes are recieved or not ?
-  if (Object.keys(req.body).length < 9) {
-    return res
-      .status(StatusCodes.PARTIAL_CONTENT)
-      .send("Some fields are missing. Please provide all the fields !!");
+  let checkPayload = true; 
+  const payload = Object.keys(req.body)
+  ROUTINE_PAYLOAD.forEach((element) => {
+    if (!payload.includes(element)) checkPayload = false; 
+  })
+
+  if(!checkPayload){
+    return res.status(StatusCodes.PARTIAL_CONTENT).send({
+      success: false,
+      message: 'Some fields are missing. Please provide all the fields !!',
+    })
   }
 
   // converting 10:00AM or 1:00PM type format to 24 hours
 
   const timeConvertor = (time) => {
-    let startingTime = time.slice(0, -2);
-    let modifiedStartingTime = 0;
+    let startingTime = time.slice(0, -2)
+    let modifiedStartingTime = 0
     // for AM format
     if (
-      time.charAt(time.length - 2) === "A" ||
-      time.charAt(time.length - 2) === "a"
+      time.charAt(time.length - 2) === 'A' ||
+      time.charAt(time.length - 2) === 'a'
     ) {
-      startingTime = time.split(":");
-      modifiedStartingTime = parseInt(startingTime[0] + startingTime[1]);
+      startingTime = time.split(':')
+      modifiedStartingTime = parseInt(startingTime[0] + startingTime[1])
     }
     // for PM format
     else {
-      startingTime = time.split(":");
-      if (startingTime[0] === "12")
-        modifiedStartingTime = parseInt(startingTime[0] + startingTime[1]);
+      startingTime = time.split(':')
+      if (startingTime[0] === '12')
+        modifiedStartingTime = parseInt(startingTime[0] + startingTime[1])
       else
         modifiedStartingTime = parseInt(
-          parseInt(startingTime[0]) + 12 + startingTime[1]
-        );
+          parseInt(startingTime[0]) + 12 + startingTime[1],
+        )
     }
-    return modifiedStartingTime;
-  };
+    return modifiedStartingTime
+  }
 
   // converting given payload to proper format
 
-  let payLoadStartTime = startTime;
-  let payLoadEndTime = endTime;
-  payLoadEndTime = timeConvertor(payLoadEndTime);
-  payLoadStartTime = timeConvertor(payLoadStartTime);
+  let payLoadStartTime = startTime
+  let payLoadEndTime = endTime
+  payLoadEndTime = timeConvertor(payLoadEndTime)
+  payLoadStartTime = timeConvertor(payLoadStartTime)
 
   const checkTime = (array, type) => {
-    let test = false;
+    let test = false
     array.forEach((element) => {
-      let startingTime = element.startTime;
-      let endingTime = element.endTime;
-      startingTime = timeConvertor(startingTime);
-      endingTime = timeConvertor(endingTime);
+      let startingTime = element.startTime
+      let endingTime = element.endTime
+      startingTime = timeConvertor(startingTime)
+      endingTime = timeConvertor(endingTime)
       if (
         (payLoadStartTime < endingTime && payLoadStartTime >= startingTime) ||
         (payLoadEndTime <= endingTime && payLoadEndTime > startingTime) ||
         (payLoadStartTime <= startingTime && payLoadEndTime >= endingTime)
       ) {
-        test = true;
+        test = true
       }
-    });
-    if (test && type === "room") {
-      return "room";
-    } else if (test && type === "teacher") {
-      return "teacher";
-    } else if (test && type === "class") {
-      return "class";
+    })
+    if (test && type === 'room') {
+      return 'room'
+    } else if (test && type === 'teacher') {
+      return 'teacher'
+    } else if (test && type === 'class') {
+      return 'class'
     } else {
-      return "none";
+      return 'none'
     }
-  };
+  }
 
   // making payload upper case
-  let modifiedBlockName = blockName.toUpperCase();
-  let modifiedRoomName = roomName.toUpperCase();
-  let modifiedDay = day.toUpperCase();
-  let modifiedModuleName = moduleName.toUpperCase();
-  let modifiedClassType = classType.toUpperCase();
-  let modifiedGroup=[]
-  if(Array.isArray(group))modifiedGroup = group.map((element) => element.toUpperCase());
+  let modifiedBlockName = blockName.toUpperCase()
+  let modifiedRoomName = roomName.toUpperCase()
+  let modifiedDay = day.toUpperCase()
+  let modifiedModuleName = moduleName.toUpperCase()
+  let modifiedClassType = classType.toUpperCase()
+  let modifiedGroup = []
+  if (Array.isArray(group))
+    modifiedGroup = group.map((element) => element.toUpperCase())
   else modifiedGroup.push(group.toUpperCase())
-  console.log(modifiedGroup)
-  
-  let modifiedTeacherName = teacherName.toUpperCase();
-  console.log(CLASS_TYPE.LECTURE)
-  console.log(modifiedClassType)
+  let modifiedTeacherName = teacherName.toUpperCase()
+
+  // check if group array has duplicate groups
+
+  const checkIfDuplicateExists = (arr) => new Set(arr).size !== arr.length
+  if (checkIfDuplicateExists(modifiedGroup))
+    return res.status(StatusCodes.BAD_REQUEST).send({
+      success: false,
+      message: 'Found duplicate group name!',
+    })
+
   // validate classType
-  if(modifiedClassType !== CLASS_TYPE.LECTURE &&
+  if (
+    modifiedClassType !== CLASS_TYPE.LECTURE &&
     modifiedClassType !== CLASS_TYPE.TUTORIAL &&
-    modifiedClassType !== CLASS_TYPE.WORKSHOP){
-      return res.status(StatusCodes.BAD_REQUEST).send({
-        success: false,
-        message: "Invalid class type!"
-      })
-    }
+    modifiedClassType !== CLASS_TYPE.WORKSHOP
+  ) {
+    return res.status(StatusCodes.BAD_REQUEST).send({
+      success: false,
+      message: 'Invalid class type!',
+    })
+  }
+
+  // validate roomName and blockName
+
+  if (modifiedBlockName !== 'HERALD' && modifiedBlockName !== 'WOLVERHAMPTON') {
+    return res.status(StatusCodes.BAD_REQUEST).send({
+      success: false,
+      message: 'Invalid Block Name!',
+    })
+  }
+
+  if (
+    (modifiedBlockName === 'HERALD' && !HCK_BLOCK.includes(modifiedRoomName)) ||
+    (modifiedBlockName === 'WOLVERHAMPTON' &&
+      !WLV_BLOCK.includes(modifiedRoomName))
+  ) {
+    return res.status(StatusCodes.BAD_REQUEST).send({
+      success: false,
+      message: 'BlockName or room name is invalid!',
+    })
+  }
 
   //logical validation
   // case 1 : check if classroom is blocked or not
@@ -117,20 +156,20 @@ const PostRoutine = async (req, res, next) => {
     blockName: modifiedBlockName,
     roomName: modifiedRoomName,
     day: modifiedDay,
-  });
+  })
   // if end time is less than start time
   if (payLoadEndTime <= payLoadStartTime) {
     return res.status(StatusCodes.BAD_REQUEST).send({
       success: false,
-      message: "end time should be greater than start time",
-    });
+      message: 'end time should be greater than start time',
+    })
   }
-  if (checkTime(check, "room") === "room") {
+  if (checkTime(check, 'room') === 'room') {
     return res.status(StatusCodes.BAD_REQUEST).send({
       success: false,
       message:
-        "The room for this particular time is already reserved for another class",
-    });
+        'The room for this particular time is already reserved for another class',
+    })
   }
 
   // case 2 : check if teacher is already assigned in another class at give time
@@ -139,16 +178,16 @@ const PostRoutine = async (req, res, next) => {
     const teacherData = await routineModel.find({
       teacherName: modifiedTeacherName,
       day: modifiedDay,
-    });
-    if (checkTime(teacherData, "teacher") === "teacher") {
+    })
+    if (checkTime(teacherData, 'teacher') === 'teacher') {
       return res.status(StatusCodes.BAD_REQUEST).send({
         success: false,
         message:
-          "The teacher is already reserved for another class in this time",
-      });
+          'The teacher is already reserved for another class in this time',
+      })
     }
-  } catch (error) {
-    console.log(error.message);
+  } catch (err) {
+    return res.status(StatusCodes.SERVICE_UNAVAILABLE).send(err)
   }
 
   // case 3: one group cannot have diferent classes on same time range
@@ -157,38 +196,38 @@ const PostRoutine = async (req, res, next) => {
     const classData = await routineModel.find({
       day: modifiedDay,
       group: { $in: modifiedGroup },
-    });
-    console.log("classData " + classData);
-    if (checkTime(classData, "class") === "class") {
+    })
+    console.log('classData ' + classData)
+    if (checkTime(classData, 'class') === 'class') {
       return res.status(StatusCodes.BAD_REQUEST).send({
         success: false,
-        message: "This group has another class in this time",
-      });
+        message: 'This group has another class in this time',
+      })
     }
-  } catch (error) {
-    console.log(error.message);
+  } catch (err) {
+    return res.status(StatusCodes.SERVICE_UNAVAILABLE).send(err)
   }
 
   // case 4: One group cannot have multiple class of same module in a single day
 
   try {
     const dayData = await routineModel.find({
-      group: {$in: modifiedGroup},
+      group: { $in: modifiedGroup },
       moduleName: modifiedModuleName,
-    });
-    console.log("dayData: "+dayData)
-    let check = false;
-    dayData.forEach(element => {
-      if(modifiedModuleName === element.moduleName) check = true;
-    });
-    if(check){
+    })
+    console.log('dayData: ' + dayData)
+    let check = false
+    dayData.forEach((element) => {
+      if (modifiedModuleName === element.moduleName) check = true
+    })
+    if (check) {
       return res.status(StatusCodes.BAD_REQUEST).send({
         success: false,
-        message: "The given group has already taken this class"
+        message: 'The given group has already taken this class',
       })
     }
-  } catch (error) {
-    console.log(error.message);
+  } catch (err) {
+    return res.status(StatusCodes.SERVICE_UNAVAILABLE).send(err)
   }
 
   //minor validation
@@ -208,72 +247,60 @@ const PostRoutine = async (req, res, next) => {
       courseType: courseType.toUpperCase(),
       moduleName: moduleName.toUpperCase(),
       teacherName: modifiedTeacherName,
-      classType: classType.toUpperCase(),
+      classType: modifiedClassType,
       group: modifiedGroup,
-      roomName: roomName.toUpperCase(),
-      blockName: blockName.toUpperCase(),
-      day: day.toUpperCase(),
+      roomName: modifiedRoomName,
+      blockName: modifiedBlockName,
+      day: modifiedDay,
       startTime: startTime,
       endTime: endTime,
-      status: "Upcoming",
+      status: 'Upcoming',
       createdOn: new Date().toLocaleDateString(),
-    });
+    })
 
     const result = await routineModel.find({
       createdOn: new Date().toLocaleDateString(),
       day: day,
       moduleName: moduleName,
-    });
+    })
     if (result.length > 0) {
       return res
         .status(404)
         .send(
-          `Routine of ${moduleName} for the date ${new Date().toLocaleDateString()} has already been created.`
-        );
-    }
-    try {
-      await data.save();
-      return res.status(StatusCodes.OK).send({
-              message: "Routine posted successfully !!",
-            });
-    } catch (error) {
-      return res.status(StatusCodes.SERVICE_UNAVAILABLE).send(err);
+          `Routine of ${moduleName} for the date ${new Date().toLocaleDateString()} has already been created.`,
+        )
     }
 
-    // data.save()
-    // .then(async () => {
-    // init the scheduler tracker
+    data
+      .save()
+      .then(async () => {
+        // init the scheduler tracker
 
-    // //upload message to notification db
-    // const notifData = new notifModel({
-    //   message: `Dear ${group} of ${courseType}, a new routine of ${moduleName} has recently published. Please see it once.`,
-    //   group: group,
-    //   createdOn: new Date().toLocaleDateString(),
-    // });
+        //upload message to notification db
+        const notifData = new notifModel({
+          message: `Dear ${modifiedGroup} of ${courseType}, a new routine of ${moduleName} has recently published. Please see it once.`,
+          group: modifiedGroup,
+          createdOn: new Date().toLocaleDateString(),
+        })
 
-    // try {
-    //   const result = await notifData.save();
-    //   if (result.message) {
-    //     pusher.trigger("my-channel", "notice", {
-    //       message: `Dear ${group} of ${courseType}, a new routine of ${moduleName} has recently published. Please see it once.`,
-    //     });
-    //     return res.status(200).send({
-    //       message: "Routine posted successfully !!",
-    //     });
-    //   }
-    // } catch (error) {
-    //   return res.status(StatusCodes.SERVICE_UNAVAILABLE).send(err);
-    // }
-    // })
-    // .catch((err) => {
-    //   console.log("hello");
-    //   return res.status(StatusCodes.SERVICE_UNAVAILABLE).send(err);
-    // });
-  } else {
-    return res
-      .status(StatusCodes.PARTIAL_CONTENT)
-      .send("Please fill all the field !!");
-  }
-};
+        try {
+          const result = await notifData.save()
+          if (result.message) {
+            pusher.trigger('my-channel', 'notice', {
+              message: `Dear ${modifiedGroup} of ${courseType}, a new routine of ${moduleName} has recently published. Please see it once.`,
+            })
+            return res.status(200).send({
+              message: 'Routine posted successfully !!',
+            })
+          }
+        } catch (error) {
+          return res.status(StatusCodes.SERVICE_UNAVAILABLE).send(err)
+        }
+      })
+      .catch((err) => {
+        return res.status(StatusCodes.SERVICE_UNAVAILABLE).send(err)
+      })
+  } 
+}
 
-module.exports = PostRoutine;
+module.exports = PostRoutine
