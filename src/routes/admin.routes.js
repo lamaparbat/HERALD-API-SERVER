cd2850ef20564ccf6616992056fd8d316a2bd626
shrawankc11 <np03cs4m210033@heraldcollege.edu.np>
2022-09-22 22:42:10 +0545

    added middleware to the route both teacher and admin

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
+const extractUser = require('../middlewares/extractUser')
 const { LOGIN, SIGNUP } = require('../controllers/index.controller').adminControllers;
 
 
 // Admin Login
-router.post('/admin/Login', LOGIN);
+router.post('/admin/Login', extractUser, LOGIN);
 
 //register new user
 router.post('/admin/Signup', SIGNUP);
