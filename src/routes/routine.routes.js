8421ca4c09767daa2e89f15dd32f76e9fb0c7bf8
RohanKumarMainali <92391187+RohanKumarMainali@users.noreply.github.com>
2022-09-23 19:23:24 +0545

    created routine Auth midlleware

--- src/routes/routine.routes.js
+++ src/routes/routine.routes.js
@@ -10,13 +10,13 @@ const {
 
 // ****** --> CRUD Routine Operation <-- *********
 //post routine data
-router.post('/admin/postRoutineData', auth.VerifyJWT(["admin"]), PostRoutine);
+router.post('/admin/postRoutineData', auth.VerifyJWT(["admin"]),auth.routineAuth(), PostRoutine);
 
 //get all routine data
 router.get('/routines', auth.VerifyJWT(["admin", "teacher","student"]), GetRoutine);
 
 //update routine data
-router.put('/admin/updateRoutineData',auth.VerifyJWT(["admin"]), UpdateRoutine )
+router.put('/admin/updateRoutineData',auth.VerifyJWT(["admin"]),UpdateRoutine )
 
 //delete routine data
 router.delete('/admin/deleteRoutineData', auth.VerifyJWT(["admin"]), DeleteRoutine)
