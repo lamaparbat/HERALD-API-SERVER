38b4278792348529ae2666df6554ad2f5e3cf210
RohanKumarMainali <92391187+RohanKumarMainali@users.noreply.github.com>
2022-09-25 20:54:14 +0545

    id sent in params

--- src/controllers/LostFound/sub-controllers/updateLostData.controller.js
+++ src/controllers/LostFound/sub-controllers/updateLostData.controller.js
@@ -1,30 +1,30 @@
-const lostFoundModel = require('../../../models/lostFoundModel');
+const lostFoundModel = require("../../../models/lostFoundModel");
 const { StatusCodes } = require("http-status-codes");
 
 const UpdateLostData = async (req, res) => {
- const { _id } = req.body;
- if(_id===undefined) return res.status(StatusCodes.BAD_REQUEST).send({
-    message: "Please provide routine ID! "
- })   
-
- try {
-  const result = await lostFoundModel.findOneAndUpdate({ _id }, {
-   isVictimRecievedData: true,
-   updatedAt: new Date().toLocaleDateString()
-  });
-  if(result === null){
-    return res.status(StatusCodes.BAD_REQUEST).send({
-        message: "Incorrect routine Id"
-    })
+  
+  const _id = req.params.id;
+  try {
+    const result = await lostFoundModel.findOneAndUpdate(
+      { _id },
+      {
+        isVictimRecievedData: true,
+        updatedAt: new Date().toLocaleDateString(),
+      }
+    );
+    if (result === null) {
+      return res.status(StatusCodes.BAD_REQUEST).send({
+        message: "Incorrect routine Id",
+      });
+    }
+    return res.status(StatusCodes.OK).send({
+      message: "Report updated successfully !!",
+    });
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      message: error.message,
+    });
   }
-  return res.status(StatusCodes.OK).send({
-   message: "Report updated successfully !!"
-  });
- } catch (error) {
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
-   message: error.message
-  });
- }
-}
+};
 
 module.exports = UpdateLostData;
