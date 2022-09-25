1fd7b3e4448c1df18dae285f8dec54007497d48d
RohanKumarMainali <92391187+RohanKumarMainali@users.noreply.github.com>
2022-09-25 18:30:35 +0545

    added routineID check

--- src/controllers/LostFound/sub-controllers/updateLostData.controller.js
+++ src/controllers/LostFound/sub-controllers/updateLostData.controller.js
@@ -3,12 +3,20 @@ const { StatusCodes } = require("http-status-codes");
 
 const UpdateLostData = async (req, res) => {
  const { _id } = req.body;
- 
+ if(_id===undefined) return res.status(StatusCodes.BAD_REQUEST).send({
+    message: "Please provide routine ID! "
+ })   
+
  try {
   const result = await lostFoundModel.findOneAndUpdate({ _id }, {
    isVictimRecievedData: true,
    updatedAt: new Date().toLocaleDateString()
   });
+  if(result === null){
+    return res.status(StatusCodes.BAD_REQUEST).send({
+        message: "Incorrect routine Id"
+    })
+  }
   return res.status(StatusCodes.OK).send({
    message: "Report updated successfully !!"
   });
