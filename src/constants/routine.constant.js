2b76d48688baf894fafcf947766288cac7bb1e69
shrawan kc <harambe@2400-1A00-B050-ip6.wlink.com.np>
2022-10-31 20:12:23 +0545

    added new constants for day and modified rooms and block constats

--- src/constants/routine.constant.js
+++ src/constants/routine.constant.js
@@ -1,46 +1,55 @@
+const ROUTINE_STATUS = [
+    'RUNNING',
+    'UPCOMING',
+    'CANCELLED',
+    'POSTPONED',
+    'COMPLETED',
+];
 
-  
-  ROUTINE_STATUS = ["RUNNING", "UPCOMING", "CANCELLED", "POSTPONED", "COMPLETED"];
-  BLOCK_NAME = { Herald: "HCK", Wolverhampton: "WLV" };
-  CHECK_IF_AVAILABLE = { room: "room", teacher: "teacher", group: "group" };
-  
-  const WLV_BLOCK_ROOMS = [
-    "LT-01 WULFRUNA",
-    "LT-03 WALSALL",
-    "SR-01 BANTOK",
-    "SR-02 BILSTON",
-    "SR-03 WOLVES",
-    "TR-02 STAFFORD",
-    "TR-03 WESTBROMWICH",
-    "LAB-01 MANDAR",
-    "LAB-02 MOSELEY",
-  ];
-  const HCK_BLOCK_ROOMS = ["BASANTAPUR", "CHANDRAGIRI", "SAGARMATHA"];
-  
-  const ROUTINE_PAYLOAD = [
-    "courseType",
-    "moduleName",
-    "teacherName",
-    "classType",
-    "group",
-    "roomName",
-    "blockName",
-    "day",
-    "startTime",
-    "endTime",
-    "status"
-  ];
-  
-  
-  module.exports = {
-    WLV_BLOCK_ROOMS,
-    HCK_BLOCK_ROOMS,
+const CHECK_IF_AVAILABLE = { room: 'room', teacher: 'teacher', group: 'group' };
+
+const ROOMS = {
+    HCK: ['BASANTAPUR', 'CHANDRAGIRI', 'SAGARMATHA'],
+    WLV: [
+        'LT-01 WULFRUNA',
+        'LT-03 WALSALL',
+        'SR-01 BANTOK',
+        'SR-02 BILSTON',
+        'SR-03 WOLVES',
+        'TR-02 STAFFORD',
+        'TR-03 WESTBROMWICH',
+        'LAB-01 MANDAR',
+        'LAB-02 MOSELEY',
+    ],
+};
+
+const ROUTINE_PAYLOAD = [
+    'courseType',
+    'moduleName',
+    'teacherName',
+    'classType',
+    'group',
+    'roomName',
+    'blockName',
+    'day',
+    'startTime',
+    'endTime',
+    'status',
+];
+
+const VALID_DAYS = [
+    'SUNDAY',
+    'MONDAY',
+    'TUESDAY',
+    'WEDNESDAY',
+    'THURSDAY',
+    'FRIDAY',
+];
+
+module.exports = {
     ROUTINE_PAYLOAD,
     ROUTINE_STATUS,
-    BLOCK_NAME,
     CHECK_IF_AVAILABLE,
-  };
-  
-  
-  
-  
+    ROOMS,
+    VALID_DAYS,
+};
