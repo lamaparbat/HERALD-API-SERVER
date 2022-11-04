6d886be1100c1e91383714bc0f34e775d91445ba
shrawan kc <harambe@2400-1A00-B050-ip6.wlink.com.np>
2022-11-04 23:54:14 +0545

    added ongoing controller to routine routes

--- src/controllers/Routines/index.controller.js
+++ src/controllers/Routines/index.controller.js
@@ -3,11 +3,12 @@ const GetRoutine = require("./sub-controllers/getRoutine.controller");
 const PostRoutine = require("./sub-controllers/postRoutine.controller");
 const UpdateRoutine = require("./sub-controllers/updateRoutine.controller");
 const DeleteRoutine = require("./sub-controllers/deleteRoutine.controller");
-
+const OngoingRoutine = require('./sub-controllers/getOngoingRoutine.controller')
 
 module.exports = {
     GetRoutine,
     PostRoutine,
     UpdateRoutine,
     DeleteRoutine,
+    OngoingRoutine,
 }
