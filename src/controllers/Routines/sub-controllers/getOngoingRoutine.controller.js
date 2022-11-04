ed73b18fa15262d7acfc3484f5ba03d5bcfe8d39
shrawan kc <harambe@2400-1A00-B050-ip6.wlink.com.np>
2022-11-05 00:02:40 +0545

    refactor

--- src/controllers/Routines/sub-controllers/getOngoingRoutine.controller.js
+++ src/controllers/Routines/sub-controllers/getOngoingRoutine.controller.js
@@ -7,25 +7,25 @@ const getOngoingRoutine = async (req, res, _next) => {
     const grp = Array.isArray(group) ? group : [group]
 
     if (group) {
-        const ongoingGroup = await routineModel.find({ group : { $in: grp }, status : { $eq: 'ONGOING' } })
-    if (ongoingGroup) {
-        return res.status(StatusCodes.OK).json({
-            success: true,
-            ongoingGroup
-        })
-    } else {
-        return res.status(StatusCodes.NOT_FOUND).json({
-            success: false,
-            message: 'this group has no ongoing classes'
-        })
+        const ongoingGroup = await routineModel.find({ group: { $in: grp }, status: { $eq: 'ONGOING' } })
+        if (ongoingGroup) {
+            return res.status(StatusCodes.OK).json({
+                success: true,
+                ongoingGroup
+            })
+        } else {
+            return res.status(StatusCodes.NOT_FOUND).json({
+                success: false,
+                message: 'this group has no ongoing classes'
+            })
+        }
     }
-}
 
-const ongoingGroups = await routineModel.find({ status: { $eq: 'ONGOING' } })
-return res.status(StatusCodes.OK).json({
-    success: true,
-    ongoingGroups
-})
+    const ongoingGroups = await routineModel.find({ status: { $eq: 'ONGOING' } })
+    return res.status(StatusCodes.OK).json({
+        success: true,
+        ongoingGroups
+    })
 }
 
 module.exports = getOngoingRoutine;
