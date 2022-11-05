c929c2b9095b7abcc9390e9447992af75c40ee64
shrawan kc <harambe@2400-1A00-B050-ip6.wlink.com.np>
2022-11-05 11:04:32 +0545

    fix #84 add ongoing routines

--- src/controllers/Routines/sub-controllers/getOngoingRoutine.controller.js
+++ src/controllers/Routines/sub-controllers/getOngoingRoutine.controller.js
@@ -2,30 +2,35 @@ const routineModel = require('../../../models/routineModel')
 
 const { StatusCodes } = require("http-status-codes");
 
-const getOngoingRoutine = async (req, res, _next) => {
+const getOngoingRoutine = async (req, res, next) => {
     const { group } = req.query
     const grp = Array.isArray(group) ? group : [group]
-
-    if (group) {
-        const ongoingGroup = await routineModel.find({ group: { $in: grp }, status: { $eq: 'ONGOING' } })
-        if (ongoingGroup) {
-            return res.status(StatusCodes.OK).json({
-                success: true,
-                ongoingGroup
-            })
-        } else {
-            return res.status(StatusCodes.NOT_FOUND).json({
-                success: false,
-                message: 'this group has no ongoing classes'
-            })
+    try {
+        if (group) {
+            const ongoingGroup = await routineModel.find({ group: { $in: grp }, status: { $eq: 'ONGOING' } })
+            if (ongoingGroup) {
+                return res.status(StatusCodes.OK).json({
+                    success: true,
+                    'ongoingClassGroup/s': ongoingGroup
+                })
+            } else {
+                return res.status(StatusCodes.NOT_FOUND).json({
+                    success: false,
+                    message: 'this group has no ongoing classes'
+                })
+            }
         }
+
+        const ongoingGroups = await routineModel.find({ status: { $eq: 'ONGOING' } })
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            ongoingGroups
+        })
+    } catch (err) {
+        next(err)
     }
 
-    const ongoingGroups = await routineModel.find({ status: { $eq: 'ONGOING' } })
-    return res.status(StatusCodes.OK).json({
-        success: true,
-        ongoingGroups
-    })
+
 }
 
 module.exports = getOngoingRoutine;
