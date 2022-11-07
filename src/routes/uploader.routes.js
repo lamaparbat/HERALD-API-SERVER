7564cd61498b6f72406a1cff3d939a4462dc32c5
shrawan kc <harambe@2400-1A00-B050-ip6.wlink.com.np>
2022-11-07 14:33:52 +0545

    made seperate multer configs

--- src/routes/uploader.routes.js
+++ src/routes/uploader.routes.js
@@ -1,43 +1,12 @@
 const router = require('express').Router();
 const auth = require("../middlewares/auth");
-const multer = require("multer");
 const { UPLOAD_STUDENT_LIST, UPLOAD_TEACHER_LIST, UPLOAD_ADMIN_LIST, UPLOAD_SCHEDULE } = require("../controllers/index.controller").uploaderControllers;
-const path = require('path');
 
-const scheduleStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, `${__dirname}/../../uploads`)
-    },
-    filename: (req, file, cb) => {
-        const date = new Date();
-        const prefix = date.toString().slice(0, 24).split(' ').join('-')
-        cb(null, prefix + '-' + file.originalname)
-    }
-})
-
-const uploadFilter = (req, file, cb) => {
-    if (path.extname(file.originalname) !== '.xlsx') cb(null, false)
-    cb(null, true)
-}
-
-const scheduleUpload = multer({ storage : scheduleStorage, fileFilter : uploadFilter }).fields([{ name : 'schedule', maxCount : 1}]);
-
-// upload college data
-const storage2 = multer.diskStorage({
- destination: (req, file, cb) => {
-  cb(null, `../../uploads`);
- },
- filename: (req, file, cb) => {
-  req.body.uploadFileName = Date.now() + "-" + file.originalname;
-  cb(null, req.body.uploadFileName);
- }
-});
-
-const collegeUpload = multer({ storage: storage2 }).single('excelfile');
+const { scheduleUpload, collegeUpload } = require('../configs/multerConfigs.config');
 
 router.post('/uploadSchedule', auth.VerifyJWT(['admin']), scheduleUpload, UPLOAD_SCHEDULE);
 
-router.post("/uploadStudentList", collegeUpload, auth.VerifyJWT(["admin"]), UPLOAD_STUDENT_LIST );
+router.post("/uploadStudentList", auth.VerifyJWT(["admin"]), collegeUpload, UPLOAD_STUDENT_LIST );
 
 
 router.post("/uploadTeacherList", auth.VerifyJWT(["admin"]), UPLOAD_TEACHER_LIST);
