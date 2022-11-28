7e0a177efd2e21143b8b79a7afa4afffd90df188
Parbat Lama <parbatlama@Parbats-MacBook-Pro.local>
2022-11-28 11:10:44 +0545

    Add mandrill webhook test api endpoints

--- src/routes/default.routes.js
+++ src/routes/default.routes.js
@@ -7,7 +7,7 @@ router.get('/', DEFAULT_ROUTES);
 
 router.post('/mailStatus', (req, res) => {
  const logs = JSON.parse(req.body.mandrill_events)[0];
-
+ console.log('ipps ', logs)
  if (!['delivered', 'hard_bounce', 'soft_bounce', 'reject'].includes(logs.event))
   res.status(200);
 
