951043b95f1e3aa142d0216a1aff248fb3c000ba
Parbat Lama <parbatlama@Parbats-MacBook-Pro.local>
2022-11-28 11:08:31 +0545

    Add mandrill webhook test api endpoints

--- src/routes/default.routes.js
+++ src/routes/default.routes.js
@@ -6,15 +6,13 @@ const DEFAULT_ROUTES = require("../controllers/default.controller");
 router.get('/', DEFAULT_ROUTES);
 
 router.post('/mailStatus', (req, res) => {
- if (!JSON.parse(req.body.mandrill_events).length)
+ const logs = JSON.parse(req.body.mandrill_events)[0];
+
+ if (!['delivered', 'hard_bounce', 'soft_bounce', 'reject'].includes(logs.event))
   res.status(200);
 
- const logs = JSON.parse(req.body.mandrill_events)[0];
- console.log(req.body.mandrill_events, logs)
- // return res.status(200);
- console.log(logs?.event)
-
  const { state, bounce_description } = logs.msg;
+
  console.log('webhook triggered ', state, bounce_description);
 
  // db query
